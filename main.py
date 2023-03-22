ccb7c5236157ae5dcb6e43ce371ceb3c3c642d85
Sergey Postnikov <ser.post2017@gmail.com>
2023-03-22 16:04:38 +0300

    check_for_redirect implemented

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from requests.exceptions import HTTPError
 import os
 from os.path import join
 from pathlib import Path
@@ -6,17 +7,14 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent
 
 
-def get_logo():
-    url = 'https://dvmn.org/assets/img/logo.8d8f24edbb5f.svg'
-    response = requests.get(url)
-    response.raise_for_status()
-
-    with open('logo.svg', 'wb') as f:
-        f.write(response.content)
+def check_for_redirect(response):
+    if response.history:
+        raise HTTPError
 
 
 def get_book(url, book_title, directory='books'):
     response = requests.get(url)
+    check_for_redirect(response)
     response.raise_for_status()
     os.makedirs(directory, exist_ok=True)
     path_to_book = join(BASE_DIR, directory, f'{book_title}.txt')
@@ -28,7 +26,10 @@ def get_book(url, book_title, directory='books'):
 def get_books(ids: list[int]):
     for book_id in ids:
         url = f'https://tululu.org/txt.php?id={book_id}'
-        get_book(url, f'{book_id}')
+        try:
+            get_book(url, f'{book_id}')
+        except HTTPError:
+            print('Book with that id, does not exist')
 
 
 def main():
