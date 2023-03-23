f77079a41428f1b70cf14f8629c751c1c7616749
Sergey Postnikov <ser.post2017@gmail.com>
2023-03-23 09:46:40 +0300

    parse book implemented

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import os
 from os.path import join
 from pathlib import Path
 
+from bs4 import BeautifulSoup
+
 BASE_DIR = Path(__file__).resolve().parent
 
 
@@ -12,7 +14,23 @@ def check_for_redirect(response):
         raise HTTPError
 
 
-def get_book(url, book_title, directory='books'):
+def parse_book(book_id):
+    url = f'https://tululu.org/b{book_id}/'
+    response = requests.get(url)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, 'lxml')
+    book_title = (
+        soup.find('h1')
+        .text
+        .replace('\xa0', '')
+        .replace(':', '')
+        )
+    # book_image = soup.find('div', class_='bookimage').find('img')['src']
+    return book_title 
+
+
+def save_book(url, book_title, directory='books'):
     response = requests.get(url)
     check_for_redirect(response)
     response.raise_for_status()
@@ -27,14 +45,16 @@ def get_books(ids: list[int]):
     for book_id in ids:
         url = f'https://tululu.org/txt.php?id={book_id}'
         try:
-            get_book(url, f'{book_id}')
+            book_title = parse_book(book_id)
+            save_book(url, book_title)
         except HTTPError:
-            print('Book with that id, does not exist')
+            print('Book with id {book_id}, does not exist')
 
 
 def main():
     ids = [i for i in range(1, 11)]
     get_books(ids)
+    # print(parse_book(32169))
 
 
 if __name__ == '__main__':
